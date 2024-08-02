fa407fbe9986a5cf4a4d8a0fc3be19a3dc27f16c
bimorajendraa <celapceluporeo@gmail.com>
2024-08-02 18:26:35 +0700

    fix: fix setup frontend

---
+++ Frontend/fe-resep/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@nextui-org/react";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
+}
